4b812e7cac2a0ba08af2772277dafe03ae952586
Acácio <99909819+acaciobjr@users.noreply.github.com>
2023-03-30 20:57:33 -0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 print("### Gerador de Preço SHIBA INU ###")
 capitalizacao = int(input('Qual o valor da capitalização em bilhões? '))
 supply = int(input('Qual o supply em questão em trilhões? '))
@@ -15,3 +18,15 @@ for i in range(1, anos_futuros+1):
     supply -= queima_anual
     preco = capitalizacao / supply
     print(f'O valor do ativo no ano {i} será de: {preco:.6f}')
+
+    
+# Envia a solicitação HTTP para o site e obtém o conteúdo HTML da página
+#url = 'https://www.shibburn.com/'
+#response = requests.get(url)
+#soup = BeautifulSoup(response.text, 'html.parser')
+
+# Encontra a seção do supply na página e extrai o valor
+#supply_section = soup.find('span', {'class': 'supply-data'}).get_text().strip()
+#supply = float(supply_section.replace('T', '')) * 1000000000000
+
+#print(f'O valor do supply é: {supply:.2f}')
